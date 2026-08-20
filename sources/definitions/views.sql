6760fff7cf303b3a074a70c1cd5f1b1aec3828c7
plamsalmoffitt <112419764+plamsalmoffitt@users.noreply.github.com>
2026-08-20 16:59:03 -0400

    Change view definition to use my_test schema

--- sources/definitions/views.sql
+++ sources/definitions/views.sql
@@ -1,8 +1,8 @@
-DEFINE VIEW order_summary AS
+DEFINE VIEW my_test.public.order_summary AS
   SELECT
     o.order_id,
     c.name AS customer_name,
     o.order_date,
     o.amount
-  FROM orders o
-  JOIN customers c ON o.customer_id = c.customer_id;
+  FROM my_test.public.orders o
+  JOIN my_test.public.customers c ON o.customer_id = c.customer_id;
