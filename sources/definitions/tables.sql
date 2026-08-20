df06f7fb27594a1f7d1c30f1043aa14ac5f22bb0
plamsalmoffitt <112419764+plamsalmoffitt@users.noreply.github.com>
2026-08-20 16:58:39 -0400

    Update tables.sql

--- sources/definitions/tables.sql
+++ sources/definitions/tables.sql
@@ -1,17 +1,17 @@
-DEFINE TABLE customers (
+DEFINE TABLE my_test.public.customers (
   customer_id NUMBER,
   name        VARCHAR,
   signup_date DATE
 );
 
-DEFINE TABLE orders (
+DEFINE TABLE my_test.public.orders (
   order_id    NUMBER,
   customer_id NUMBER,
   order_date  DATE,
   amount      NUMBER(10,2)
 );
 
-DEFINE TABLE products (
+DEFINE TABLE my_test.public.products (
   product_id NUMBER,
   name       VARCHAR,
   price      NUMBER(10,2)
